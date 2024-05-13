4b404d7b3b001cf1ac95ac4b6d6a913312baf1ed
Ilan Gibson <ilanjgibson@gmail.com>
2024-05-13 11:37:13 -0600

    clean up routes and route names.

--- src/main.rs
+++ src/main.rs
@@ -6,31 +6,20 @@ use std::path::PathBuf;
 #[macro_use] extern crate rocket;
 
 #[get("/<file..>", rank = 2)] // Define a catch-all route for serving static files
-async fn static_files(file: PathBuf) -> Option<NamedFile> {
+async fn serve_static_files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).await.ok()
 }
 
 #[get("/")]
-async fn index() -> Option<NamedFile> {
+async fn serve_initial_page() -> Option<NamedFile> {
     NamedFile::open("static/html/initial_page.html").await.ok()
-}
-
-#[get("/<code>")]
-async fn my_handler(code: &str) -> Option<NamedFile> {
-    println!("code: {}", code);
-    NamedFile::open("static/html/initial_page.html").await.ok()
-}
-
-#[get("/world")]
-async fn world() -> &'static str {
-    "Hello, world!"
 }
 
 // #[tokio::main]
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
     let _rocket = rocket::build()
-        .mount("/", routes![index, static_files, my_handler, world])
+        .mount("/", routes![serve_initial_page, serve_static_files])
         .launch()
         .await?;
 
